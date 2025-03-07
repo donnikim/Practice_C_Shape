9dcd5c3f3e9d83027e2eb8ccfe85dacbb625ba4f
donnikim <dongil0913@gmail.com>
2025-03-07 17:41:11 +0900

    Study_user32.dll

--- Day10/39_Study_User32_CopyAndPaste/39_Study_User32_CopyAndPaste/Form1.Designer.cs
+++ Day10/39_Study_User32_CopyAndPaste/39_Study_User32_CopyAndPaste/Form1.Designer.cs
@@ -31,29 +31,137 @@ namespace _39_Study_User32_CopyAndPaste
         private void InitializeComponent()
         {
             this.lboxTestSave = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblactivation = new System.Windows.Forms.Label();
+            this.cboxactivation = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Tbar = new System.Windows.Forms.TrackBar();
+            this.btnlbtextadd = new System.Windows.Forms.Button();
+            this.txtlbtextadd = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Tbar)).BeginInit();
             this.SuspendLayout();
             // 
             // lboxTestSave
             // 
+            this.lboxTestSave.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lboxTestSave.FormattingEnabled = true;
             this.lboxTestSave.ItemHeight = 12;
-            this.lboxTestSave.Location = new System.Drawing.Point(12, 22);
+            this.lboxTestSave.Location = new System.Drawing.Point(3, 43);
             this.lboxTestSave.Name = "lboxTestSave";
-            this.lboxTestSave.Size = new System.Drawing.Size(306, 304);
+            this.lboxTestSave.Size = new System.Drawing.Size(300, 240);
             this.lboxTestSave.TabIndex = 0;
-            this.lboxTestSave.SelectedIndexChanged += LboxTestSave_SelectIndexChanged;
-
+            this.lboxTestSave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbox_keyDown);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lboxTestSave, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(306, 326);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblactivation);
+            this.panel1.Controls.Add(this.cboxactivation);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblactivation
+            // 
+            this.lblactivation.AutoSize = true;
+            this.lblactivation.Location = new System.Drawing.Point(99, 13);
+            this.lblactivation.Name = "lblactivation";
+            this.lblactivation.Size = new System.Drawing.Size(149, 12);
+            this.lblactivation.TabIndex = 1;
+            this.lblactivation.Text = "활성화 상태 (Ctrl+ C 가능)";
+            // 
+            // cboxactivation
+            // 
+            this.cboxactivation.AutoSize = true;
+            this.cboxactivation.Checked = true;
+            this.cboxactivation.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cboxactivation.Location = new System.Drawing.Point(9, 12);
+            this.cboxactivation.Name = "cboxactivation";
+            this.cboxactivation.Size = new System.Drawing.Size(78, 16);
+            this.cboxactivation.TabIndex = 0;
+            this.cboxactivation.Text = "Activation";
+            this.cboxactivation.UseVisualStyleBackColor = true;
+            this.cboxactivation.CheckedChanged += new System.EventHandler(this.cbox_Change);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Tbar);
+            this.panel2.Controls.Add(this.btnlbtextadd);
+            this.panel2.Controls.Add(this.txtlbtextadd);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 286);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(306, 40);
+            this.panel2.TabIndex = 2;
+            // 
+            // Tbar
+            // 
+            this.Tbar.Location = new System.Drawing.Point(205, 3);
+            this.Tbar.Minimum = 2;
+            this.Tbar.Name = "Tbar";
+            this.Tbar.Size = new System.Drawing.Size(86, 45);
+            this.Tbar.TabIndex = 2;
+            this.Tbar.Value = 10;
+            this.Tbar.Scroll += new System.EventHandler(this.tbox_scroll);
+            // 
+            // btnlbtextadd
+            // 
+            this.btnlbtextadd.Location = new System.Drawing.Point(101, 7);
+            this.btnlbtextadd.Name = "btnlbtextadd";
+            this.btnlbtextadd.Size = new System.Drawing.Size(75, 23);
+            this.btnlbtextadd.TabIndex = 1;
+            this.btnlbtextadd.Text = "등록";
+            this.btnlbtextadd.UseVisualStyleBackColor = true;
+            this.btnlbtextadd.Click += new System.EventHandler(this.btnlbtextadd_Click);
+            // 
+            // txtlbtextadd
+            // 
+            this.txtlbtextadd.Location = new System.Drawing.Point(11, 9);
+            this.txtlbtextadd.Name = "txtlbtextadd";
+            this.txtlbtextadd.Size = new System.Drawing.Size(84, 21);
+            this.txtlbtextadd.TabIndex = 0;
+            this.txtlbtextadd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbox_KeyDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 350);
-            this.Controls.Add(this.lboxTestSave);
+            this.ClientSize = new System.Drawing.Size(330, 345);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Tbar)).EndInit();
             this.ResumeLayout(false);
-            this.Load += Form1_Load;
 
         }
 
@@ -61,6 +169,14 @@ namespace _39_Study_User32_CopyAndPaste
         #endregion
 
         private System.Windows.Forms.ListBox lboxTestSave;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblactivation;
+        private System.Windows.Forms.CheckBox cboxactivation;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar Tbar;
+        private System.Windows.Forms.Button btnlbtextadd;
+        private System.Windows.Forms.TextBox txtlbtextadd;
     }
 }
 
--- Day10/39_Study_User32_CopyAndPaste/39_Study_User32_CopyAndPaste/Form1.cs
+++ Day10/39_Study_User32_CopyAndPaste/39_Study_User32_CopyAndPaste/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -23,14 +25,42 @@ namespace _39_Study_User32_CopyAndPaste
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Version oVersion = Assembly.GetEntryAssembly().GetName().Version;
+            this.Text = string.Format("{0} Ver.{1}.{2} / Build Time ({3})", "Copy&Paste", oVersion.Major, oVersion.Minor, GetBuildDataTime(oVersion));
+     
             _kbHook.hook();
             _kbHook.KeyDown += _kbHook_KeyDown;   
         }
 
+        // Build Time을 변환        
+        public DateTime GetBuildDataTime(Version oVersion)
+        {
+            string strVerstion = oVersion.ToString();             // 날짜 등록            
+            int iDays = Convert.ToInt32(strVerstion.Split('.')[2]);
+            DateTime refData = new DateTime(2000, 1, 1);
+            DateTime dtBuildDate = refData.AddDays(iDays);             // 초 등록            
+            int iSeconds = Convert.ToInt32(strVerstion.Split('.')[3]);
+            iSeconds = iSeconds * 2;
+            dtBuildDate = dtBuildDate.AddSeconds(iSeconds);             // 시차 조정            
+            DaylightTime daylighttime = TimeZone.CurrentTimeZone.GetDaylightChanges(dtBuildDate.Year);
+            if (TimeZone.IsDaylightSavingTime(dtBuildDate, daylighttime))
+            {
+                dtBuildDate = dtBuildDate.Add(daylighttime.Delta);
+            }
+            return dtBuildDate;
+        }
+     
+
         private void _kbHook_KeyDown(Object sender,KeyEventArgs e)
         {
-            if(e.Control && e.KeyCode == Keys.C)
+            if(e.Control && e.KeyCode == Keys.C && cboxactivation.Checked)
             {
+                string strText = Clipboard.GetData(System.Windows.Forms.DataFormats.UnicodeText).ToString();
+                if (lboxTestSave.Items.Contains(strText))
+                {
+                    return;
+                }
+
                 Thread.Sleep(400);
                 lboxTestSave.Items.Add(Clipboard.GetData(System.Windows.Forms.DataFormats.UnicodeText).ToString());
             }
@@ -45,6 +75,61 @@ namespace _39_Study_User32_CopyAndPaste
             }
         }
 
+        private void tbox_scroll(object sender, EventArgs e)
+        {
+            this.Opacity = Tbar.Value / 10.0;
+        }
 
+        private void btnlbtextadd_Click(object sender, EventArgs e)
+        {
+            DataSave();
+        }
+
+        private void tbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                DataSave();
+            }
+        }
+
+        private void DataSave()
+        {
+
+            string strText = txtlbtextadd.Text;
+            
+            if (!String.IsNullOrEmpty(strText) && !lboxTestSave.Items.Contains(strText))
+            {
+                lboxTestSave.Items.Add(strText);
+                txtlbtextadd.Text = "";
+            }
+        }
+
+        private void cbox_Change(object sender, EventArgs e)
+        {
+            if(cboxactivation.Checked)
+            {
+                lblactivation.Text = "활성화 상태 (Ctrl+ C 가능)";
+                lblactivation.Enabled = true;
+                txtlbtextadd.Enabled = true;
+                btnlbtextadd.Enabled = true;
+
+            }
+            else
+            {
+                lblactivation.Text = "비활성화 상태 (붙여넣기만 가능)";
+                lblactivation.Enabled = false;
+                txtlbtextadd.Enabled = false;
+                btnlbtextadd.Enabled = false;
+            }
+        }
+
+        private void lbox_keyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                lboxTestSave.Items.RemoveAt(lboxTestSave.SelectedIndex);
+            }
+        }
     }
 }
